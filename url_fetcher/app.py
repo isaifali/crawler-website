4b7af1179c8f0473abf169ab4813075fe1eed4b3
Anuj <97934493+AnujnautiyalCS@users.noreply.github.com>
2023-12-21 13:48:59 +0530

    first commit

---
+++ url_fetcher/app.py
@@ -0,0 +1,346 @@
+from flask import Flask, render_template, request, send_file
+from urllib.parse import urljoin, urlparse
+from bs4 import BeautifulSoup
+from pdf2docx import Converter
+import requests
+from PIL import Image
+import pytesseract
+import io
+import os
+import time
+import threading
+import pandas as pd 
+import zipfile
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Dictionary to store upload timestamps
+file_upload_times = {}
+
+# Dictionary to store cleanup schedules
+cleanup_schedules = {}
+
+
+def compress_image(image_path, output_path, quality=85):
+    try:
+        img = Image.open(image_path)
+
+        # Check image format
+        if img.format == 'JPEG':
+            # For JPEG, use the quality parameter
+            img.save(output_path, 'JPEG', quality=quality)
+        elif img.format == 'PNG':
+            # For PNG, use a different approach (quantization)
+            img = img.convert("P", palette=Image.ADAPTIVE, colors=256)
+            img.save(output_path, 'PNG', optimize=True)
+
+    except Exception as e:
+        print(f"Error compressing image: {str(e)}")
+
+
+def schedule_cleanup(pdf_path, word_path):
+    word_filename = os.path.basename(word_path)
+
+    # Wait for 1 minute before cleanup
+    time.sleep(60)
+
+    # Delete the PDF and Word files
+    os.remove(pdf_path)
+    os.remove(word_path)
+
+    # Remove the schedule entry
+    del cleanup_schedules[word_filename]
+
+
+def schedule_temporary_cleanup(file_path):
+    # Wait for 5 minutes before cleanup
+    time.sleep(300)
+
+    # Delete the temporary file
+    os.remove(file_path)
+
+
+@app.route('/compress_image')
+def compress_images_page():
+    return render_template('compress_image.html')
+
+
+@app.route('/upload_and_compress', methods=['POST'])
+def upload_and_compress():
+    if request.method == 'POST':
+        quality = int(request.form['quality'])
+        uploaded_files = request.files.getlist('images')
+
+        if uploaded_files:
+            # Check if only one image is selected
+            if len(uploaded_files) == 1:
+                # Only one image, provide it as a download without zipping
+                uploaded_file = uploaded_files[0]
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+                uploaded_file.save(image_path)
+
+                compressed_filename = f"compressed_{uploaded_file.filename}"
+                compressed_path = os.path.join(app.config['UPLOAD_FOLDER'], compressed_filename)
+                compress_image(image_path, compressed_path, quality=quality)
+
+                # Schedule cleanup for temporary files after 5 minutes
+                cleanup_thread = threading.Thread(target=schedule_temporary_cleanup, args=(image_path,))
+                cleanup_thread.start()
+
+                # Provide the compressed image as a download
+                return send_file(compressed_path, as_attachment=True, download_name=compressed_filename)
+
+            else:
+                # Multiple images, create a zip file
+                # Create a temporary directory to store compressed images
+                temp_dir = 'temp_images'
+                os.makedirs(temp_dir, exist_ok=True)
+
+                # Compress each uploaded image
+                compressed_files = []
+                for uploaded_file in uploaded_files:
+                    image_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+                    uploaded_file.save(image_path)
+
+                    compressed_filename = f"compressed_{uploaded_file.filename}"
+                    compressed_path = os.path.join(temp_dir, compressed_filename)
+                    compress_image(image_path, compressed_path, quality=quality)
+
+                    compressed_files.append(compressed_path)
+
+                # Create a zip file containing all compressed images
+                zip_filename = 'compressed_images.zip'
+                zip_path = os.path.join(app.config['UPLOAD_FOLDER'], zip_filename)
+                with zipfile.ZipFile(zip_path, 'w') as zip_file:
+                    for compressed_file in compressed_files:
+                        zip_file.write(compressed_file, os.path.basename(compressed_file))
+
+                # Schedule cleanup for temporary files after 5 minutes
+                cleanup_thread = threading.Thread(target=schedule_temporary_cleanup, args=(temp_dir,))
+                cleanup_thread.start()
+
+                # Provide the zip file as a download
+                return send_file(zip_path, as_attachment=True, download_name=zip_filename)
+
+    return render_template('compress_image.html')
+
+
+
+def cleanup_files(pdf_path, word_path):
+    # Delete the PDF and Word files
+    os.remove(pdf_path)
+    os.remove(word_path)
+
+
+
+def html_table_to_csv(html_content):
+    # Use BeautifulSoup to parse HTML content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Find the HTML table
+    table = soup.find('table')
+
+    # Use pandas to read the HTML table and convert it to CSV
+    df = pd.read_html(str(table), header=0)[0]
+    csv_content = df.to_csv(index=False)
+
+    return csv_content
+
+@app.route('/html_to_csv', methods=['GET', 'POST'])
+def html_to_csv():
+    if request.method == 'POST':
+        html_content = request.form.get('html_content')
+        if html_content:
+            csv_content = html_table_to_csv(html_content)
+
+            # Provide the CSV file as a download
+            return send_file(io.BytesIO(csv_content.encode()), as_attachment=True, download_name='table.csv')
+    else:
+        # Your code for handling GET requests
+        return render_template('html_to_csv.html')
+
+    return render_template('html_to_csv.html')
+
+@app.route('/pdf-to-doc')
+def pdf_to_doc():
+    return render_template('pdf-to-doc.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    if request.method == 'POST':
+        # Check if the POST request has the file part
+        if 'file' not in request.files:
+            return render_template('pdf-to-doc.html', error='No file part')
+
+        file = request.files['file']
+
+        # Check if the file is selected
+        if file.filename == '':
+            return render_template('pdf-to-doc.html', error='No file selected')
+
+        # Check if the file is a PDF
+        if file.filename.endswith('.pdf'):
+            # Convert PDF to Word
+            word_filename = file.filename.replace('.pdf', '.docx')
+            pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            word_path = os.path.join(app.config['UPLOAD_FOLDER'], word_filename)
+
+            file.save(pdf_path)
+
+            cv = Converter(pdf_path)
+            cv.convert(word_path)
+            cv.close()
+
+            # Store the current timestamp for file cleanup
+            file_upload_times[word_filename] = time.time()
+
+            # Start a thread to schedule file cleanup after 1 minute
+            cleanup_thread = threading.Thread(target=schedule_cleanup, args=(pdf_path, word_path))
+            cleanup_thread.start()
+
+            return send_file(word_path, as_attachment=True, download_name=word_filename)
+
+        else:
+            return render_template('pdf-to-doc.html', error='Invalid file format. Please upload a PDF file.')
+        
+def schedule_cleanup(pdf_path, word_path):
+    word_filename = os.path.basename(word_path)
+    upload_time = file_upload_times.get(word_filename, 0)
+
+    # Wait for 1 minute after the file upload time
+    while time.time() - upload_time < 60:
+        time.sleep(1)
+
+    cleanup_files(pdf_path, word_path)
+
+
+def bfs_fetch_urls(start_url, allowed_domain, max_urls=1000):
+    visited_urls = set()
+    unique_urls = set()
+    queue = [start_url]
+    urls_fetched = 0
+
+    while queue and urls_fetched < max_urls:
+        current_url = queue.pop(0)
+
+        # Skip if the URL has already been visited
+        if current_url in visited_urls:
+            continue
+
+        try:
+            response = requests.get(current_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Extract all anchor tags
+            anchor_tags = soup.find_all('a', href=True)
+
+            for anchor_tag in anchor_tags:
+                href = anchor_tag['href']
+                resolved_url = urljoin(start_url, href)
+
+                # Check if the resolved URL starts with the allowed domain
+                if resolved_url.startswith(allowed_domain):
+                    unique_urls.add(resolved_url)
+
+                    # Add the resolved URL to the queue if it hasn't been visited
+                    if resolved_url not in visited_urls:
+                        queue.append(resolved_url)
+                        urls_fetched += 1
+
+            
+        except Exception as e:
+            print(f"Error fetching URLs for {current_url}: {str(e)}")
+
+        visited_urls.add(current_url)
+
+    return list(unique_urls)
+
+
+def extract_text_from_image(image_content):
+    try:
+        img = Image.open(io.BytesIO(image_content))
+        
+        # Specify the path to the Tesseract executable
+        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+        
+        text = pytesseract.image_to_string(img)
+        return text
+    except Exception as e:
+        print(f"Error extracting text from image: {str(e)}")
+        return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    urls = []
+
+    if request.method == 'POST':
+        start_url = request.form.get('url')
+        if start_url:
+            allowed_domain = urlparse(start_url).scheme + '://' + urlparse(start_url).netloc
+            urls = bfs_fetch_urls(start_url, allowed_domain)
+
+            # For demonstration, add a delay to simulate fetching
+            # time.sleep(5)
+
+    return render_template('index.html', urls=urls)
+
+@app.route('/fetch_urls', methods=['POST'])
+def fetch_urls():
+    if request.method == 'POST':
+        start_url = request.form.get('url')
+        if start_url:
+            allowed_domain = urlparse(start_url).scheme + '://' + urlparse(start_url).netloc
+            urls = bfs_fetch_urls(start_url, allowed_domain)
+
+            # For demonstration, add a delay to simulate fetching
+            # time.sleep(5)
+
+            return {'urls': urls}
+
+    return render_template('index.html')
+
+@app.route('/image_to_text', methods=['GET', 'POST'])
+def image_to_text():
+    extracted_text = ""
+
+    if request.method == 'POST':
+        image_file = request.files['image']
+
+        if image_file:
+            image_content = image_file.read()
+            extracted_text = extract_text_from_image(image_content)
+
+    return render_template('image_to_text.html', extracted_text=extracted_text)
+
+
+
+@app.route('/jpg_to_png')
+def jpg_to_png():
+    return render_template('jpg_to_png.html')
+
+@app.route('/convert_jpg_to_png', methods=['POST'])
+def convert_jpg_to_png():
+    if request.method == 'POST':
+        jpg_file = request.files['jpg_file']
+
+        if jpg_file:
+            # Save the JPG file
+            jpg_path = os.path.join(app.config['UPLOAD_FOLDER'], jpg_file.filename)
+            jpg_file.save(jpg_path)
+
+            # Convert JPG to PNG
+            png_filename = jpg_file.filename.replace('.jpg', '.png')
+            png_path = os.path.join(app.config['UPLOAD_FOLDER'], png_filename)
+
+            img = Image.open(jpg_path)
+            img.save(png_path, 'PNG')
+
+            # Provide the PNG file as a download
+            return send_file(png_path, as_attachment=True, download_name=png_filename)
+
+    return render_template('jpg_to_png.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ url_fetcher/static/js/main.js
@@ -0,0 +1,412 @@
+/**
+* Template Name: HeroBiz
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/herobiz-bootstrap-business-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+document.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Sticky header on scroll
+   */
+  const selectHeader = document.querySelector('#header');
+  if (selectHeader) {
+    document.addEventListener('scroll', () => {
+      window.scrollY > 100 ? selectHeader.classList.add('sticked') : selectHeader.classList.remove('sticked');
+    });
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = document.querySelectorAll('#navbar .scrollto');
+
+  function navbarlinksActive() {
+    navbarlinks.forEach(navbarlink => {
+
+      if (!navbarlink.hash) return;
+
+      let section = document.querySelector(navbarlink.hash);
+      if (!section) return;
+
+      let position = window.scrollY;
+      if (navbarlink.hash != '#header') position += 200;
+
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active');
+      } else {
+        navbarlink.classList.remove('active');
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive);
+  document.addEventListener('scroll', navbarlinksActive);
+
+  /**
+   * Function to scroll to an element with top ofset
+   */
+  function scrollto(el) {
+    const selectHeader = document.querySelector('#header');
+    let offset = 0;
+
+    if (selectHeader.classList.contains('sticked')) {
+      offset = document.querySelector('#header.sticked').offsetHeight;
+    } else if (selectHeader.hasAttribute('data-scrollto-offset')) {
+      offset = selectHeader.offsetHeight - parseInt(selectHeader.getAttribute('data-scrollto-offset'));
+    }
+    window.scrollTo({
+      top: document.querySelector(el).offsetTop - offset,
+      behavior: 'smooth'
+    });
+  }
+
+  /**
+   * Fires the scrollto function on click to links .scrollto
+   */
+  let selectScrollto = document.querySelectorAll('.scrollto');
+  selectScrollto.forEach(el => el.addEventListener('click', function(event) {
+    if (document.querySelector(this.hash)) {
+      event.preventDefault();
+
+      let mobileNavActive = document.querySelector('.mobile-nav-active');
+      if (mobileNavActive) {
+        mobileNavActive.classList.remove('mobile-nav-active');
+
+        let navbarToggle = document.querySelector('.mobile-nav-toggle');
+        navbarToggle.classList.toggle('bi-list');
+        navbarToggle.classList.toggle('bi-x');
+      }
+      scrollto(this.hash);
+    }
+  }));
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (document.querySelector(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+   * Mobile nav toggle
+   */
+  const mobileNavToogle = document.querySelector('.mobile-nav-toggle');
+  if (mobileNavToogle) {
+    mobileNavToogle.addEventListener('click', function(event) {
+      event.preventDefault();
+
+      document.querySelector('body').classList.toggle('mobile-nav-active');
+
+      this.classList.toggle('bi-list');
+      this.classList.toggle('bi-x');
+    });
+  }
+
+  /**
+   * Toggle mobile nav dropdowns
+   */
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+
+  navDropdowns.forEach(el => {
+    el.addEventListener('click', function(event) {
+      if (document.querySelector('.mobile-nav-active')) {
+        event.preventDefault();
+        this.classList.toggle('active');
+        this.nextElementSibling.classList.toggle('dropdown-active');
+
+        let dropDownIndicator = this.querySelector('.dropdown-indicator');
+        dropDownIndicator.classList.toggle('bi-chevron-up');
+        dropDownIndicator.classList.toggle('bi-chevron-down');
+      }
+    })
+  });
+
+  /**
+   * Auto generate the hero carousel indicators
+   */
+  let heroCarouselIndicators = document.querySelector('#hero .carousel-indicators');
+  if (heroCarouselIndicators) {
+    let heroCarouselItems = document.querySelectorAll('#hero .carousel-item')
+
+    heroCarouselItems.forEach((item, index) => {
+      if (index === 0) {
+        heroCarouselIndicators.innerHTML += `<li data-bs-target="#hero" data-bs-slide-to="${index}" class="active"></li>`;
+      } else {
+        heroCarouselIndicators.innerHTML += `<li data-bs-target="#hero" data-bs-slide-to="${index}"></li>`;
+      }
+    });
+  }
+
+  /**
+   * Scroll top button
+   */
+  const scrollTop = document.querySelector('.scroll-top');
+  if (scrollTop) {
+    const togglescrollTop = function() {
+      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+    }
+    window.addEventListener('load', togglescrollTop);
+    document.addEventListener('scroll', togglescrollTop);
+    scrollTop.addEventListener('click', window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    }));
+  }
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  let portfolionIsotope = document.querySelector('.portfolio-isotope');
+
+  if (portfolionIsotope) {
+
+    let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
+    let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
+    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+
+    window.addEventListener('load', () => {
+      let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
+        itemSelector: '.portfolio-item',
+        layoutMode: portfolioLayout,
+        filter: portfolioFilter,
+        sortBy: portfolioSort
+      });
+
+      let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+      menuFilters.forEach(function(el) {
+        el.addEventListener('click', function() {
+          document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
+          this.classList.add('filter-active');
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter')
+          });
+          if (typeof aos_init === 'function') {
+            aos_init();
+          }
+        }, false);
+      });
+
+    });
+
+  }
+
+  /**
+   * Clients Slider
+   */
+  new Swiper('.clients-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120
+      }
+    }
+  });
+
+  /**
+   * Testimonials Slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Testimonials Slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+});
+
+function toggleLoadingIndicator(show) {
+  var loadingIndicator = document.getElementById('loading-indicator');
+  loadingIndicator.style.display = show ? 'block' : 'none';
+}
+
+// Function to fetch URLs
+function fetchUrls() {
+  var url = document.getElementById('url').value;
+
+  // Show loading indicator
+  toggleLoadingIndicator(true);
+
+  fetch('/fetch_urls', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+          'url': url,
+      }),
+  })
+      .then(response => response.json())
+      .then(data => {
+          // Hide loading indicator
+          toggleLoadingIndicator(false);
+
+          // Display URLs in the table
+          var urlTable = document.getElementById('url-table');
+          var urlTableBody = document.getElementById('url-table-body');
+          urlTable.style.display = 'inline-table';
+          urlTableBody.innerHTML = '';
+
+          data.urls.forEach((url, index) => {
+              var row = urlTableBody.insertRow();
+              var serialNumberCell = row.insertCell(0);
+              var urlCell = row.insertCell(1);
+
+              serialNumberCell.textContent = index + 1;
+              urlCell.textContent = url;
+          });
+      })
+      .catch(error => {
+          console.error('Error fetching URLs:', error);
+
+          // Hide loading indicator in case of an error
+          toggleLoadingIndicator(false);
+      });
+}
+
+
+function clearDefaultText() {
+  var urlInput = document.getElementById('url');
+  if (urlInput.value === "enter url here") {
+      urlInput.value = "";
+  }
+}
+
+document.getElementById('downloadBtn').addEventListener('click', function () {
+        // Get the table HTML content
+        var table = document.getElementById('urlTable');
+        var html = table.outerHTML;
+
+        // Convert the HTML content to Blob
+        var blob = new Blob([html], { type: 'application/vnd.ms-excel' });
+        var url = URL.createObjectURL(blob);
+
+        // Create a download link and trigger click
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = 'fetched_urls.xls';
+        document.body.appendChild(a);
+        a.click();
+
+        // Remove the download link
+        document.body.removeChild(a);
+    });
+    function handleDragOver(event) {
+      event.preventDefault();
+      document.getElementById('drop-area').classList.add('highlight');
+  }
+
+  function handleDrop(event) {
+      event.preventDefault();
+      document.getElementById('drop-area').classList.remove('highlight');
+
+      const files = event.dataTransfer.files;
+      if (files.length > 0) {
+          handleImage(files[0]);
+      }
+  }
+
+  function handleFileInput(input) {
+      const files = input.files;
+      if (files.length > 0) {
+          handleImage(files[0]);
+      }
+  }
+
+  function handleImage(file) {
+      if (file.type.startsWith('image/')) {
+          const reader = new FileReader();
+
+          reader.onload = function (e) {
+              const previewImage = document.getElementById('preview-image');
+              previewImage.src = e.target.result;
+              previewImage.style.display = 'block';
+          };
+
+          reader.readAsDataURL(file);
+      } else {
+          alert('Please choose a valid image file.');
+      }
+  }S
